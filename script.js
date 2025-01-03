6975a558eb264c1c812030c14f8d12b24fc02dd8
yucann <122705177+yucann@users.noreply.github.com>
2025-01-03 18:41:05 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelectorAll('nav ul li a').forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        target.scrollIntoView({ behavior: 'smooth' });
+    });
+});
